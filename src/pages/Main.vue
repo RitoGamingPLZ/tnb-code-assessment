3bed754534196c3930cea06def4e189b991478d1
Alan Chiu <alan@gmail.com>
2023-10-01 01:20:06 +0800

    feat: completed basic layout of website (TODO: responiveness, font family)

--- src/pages/Main.vue
+++ src/pages/Main.vue
@@ -15,7 +15,7 @@
       </section>
 
       <!-- Introduction section -->
-      <section class="w-full px-17.5 space-y-10 py-10">
+      <section class="w-full px-17.5 space-y-10 -translate-y-12">
         <!-- What's New Block -->
         <div>
           <h4 class=" text-lg font-bold text-white">What's New</h4>
@@ -59,9 +59,12 @@
       </section>
 
       <!-- Learn more about course section -->
-      <section class="w-full px-17.5 space-y-10 py-10 bg-primary-900">
-        <h3 class="w-full text-center text-white font-bold text-3xl">Learn at your own pace, online or in-person</h3>
-
+      <section class="w-full px-17.5 space-y-10 py-12 bg-primary-900">
+        <div class="grid grid-cols-12 gap-5">
+          <h3 class="col-span-10 col-start-2 text-white  text-3xl text-center">
+            Learn at your own pace, online or in-person
+          </h3>
+        </div>
         <!-- Selection Block -->
         <div>
           <h4 class="text-white font-bold text-lg mb-5">Tell use who you are and we will suggest training for you</h4>
@@ -100,7 +103,7 @@
             <button class="text-white ">See all training »</button>
           </div>
           <div class=" grid grid-cols-12 gap-5">
-            <button v-for="i in 8" :key="i" class="col-span-3 bg-primary-100 rounded-4 p-2.5 text-primary-900">
+            <button v-for="i in 8" :key="i" class="col-span-3 bg-primary-100 rounded-4 p-3 text-primary-900">
               Training Topic
             </button>
           </div>
@@ -128,8 +131,12 @@
       </section>
 
       <!-- Goal Section -->
-      <section class="relative px-17.5 space-y-10 py-10">
-        <h3 class="text-center text-3xl font-bold">At TEN, our goal is to</h3>
+      <section class="relative px-17.5 space-y-10 py-12">
+        <div class="grid grid-cols-12 gap-5">
+          <h3 class="col-span-8 col-start-3  text-3xl text-center">
+            At TEN, our goal is to
+          </h3>
+        </div>
 
         <!-- Video List Block -->
         <div>
@@ -147,10 +154,51 @@
           <button class="text-primary-900">Learn more about TEN »</button>
         </div>
       </section>
+
+      <!-- Member Section -->
+      <section class="relative px-17.5 space-y-10 py-12 bg-[url('src/assets/images/background-1.png')] bg-cover">
+        <div class="grid grid-cols-12 gap-5">
+          <h3 class="col-span-8 col-start-3 text-white text-3xl text-center">
+            TEN training is already helping operators, communities, and destinations like you
+          </h3>
+        </div>
+
+        <!-- Member Block -->
+        <div class="grid grid-cols-12 gap-5">
+          <article v-for="i in 3" :key="i" class="col-span-4 flex flex-col aspect-[0.83] bg-neutral-50 p-5 space-y-5">
+            <img src="src/assets/images/background-1.png" class="mx-auto w-36 aspect-square rounded-full">
+            <p class="text-center flex-1">
+              When I read through best practices, I see a checklist of what I have to do to have a good visitor experience in terms of a community, destination or route. That’s a really powerful thing.
+            </p>
+            <h4 class="text-center font-bold">Dr. Rebecca Pollock</h4>
+            <p class="text-center font-bold">Executive Director, <span class="text-primary-700">Georgian Bay Biosphere Reserve</span></p>
+          </article>
+        </div>
+
+        <div class="grid grid-cols-12 gap-5">
+          <h3 class="col-span-8 col-start-3 text-white text-3xl text-center">
+            Get started
+          </h3>
+        </div>
+        <div class="w-fit mx-auto">
+          <button class="text-primary-700 bg-white rounded-4 px-5 py-1.5">Enroll »</button>
+        </div>
+      </section>
     </main>
 
-    <footer class=" bg-neutral-100 w-full h-[215px]">
+    <footer class="relative px-17.5 space-y-10 py-12 bg-neutral-100 w-full h-[215px]">
+      <div class="grid grid-cols-12 gap-5">
+        <div class=" col-span-8 col-start-3 -mx-2.5 flex">
+          <div class="flex justify-center items-center flex-[5]">
+            <h4 class="font-bold">Still not sure what TEN can do for you? </h4>
+          </div>
 
+          <div class="flex flex-col justify-center items-center flex-3 space-y-5">
+            <button class="rounded-4 w-full h-10 bg-primary-700 text-white px-3 py-1">Talk to a broker in your area »</button>
+            <button class="rounded-4 w-full h-10 bg-primary-700 text-white px-3 py-1">Check out our FREE resources »</button>
+          </div>
+        </div>
+      </div>
     </footer>
     <div class="absolute inset-0 grid grid-cols-12 px-17.5 gap-5 pointer-events-none">
       <div v-for="i in 12" :key="i" class="bg-red-600/10 col-span-1 h-full"></div>
